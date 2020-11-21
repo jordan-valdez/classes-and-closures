4b33f7a27b7513eb8619b052e7c99d625a445fdc
Jordan Valdez <jordanaustinvaldez@gmail.com>
2020-11-21 16:32:39 -0700

    completed

--- closures.js
+++ closures.js
@@ -22,13 +22,13 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+const inner = outer()// Code Here
 
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner() //Code Here
 
 
 
@@ -51,7 +51,7 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+const callJake = callFriend(`Jake`, 435-555-9248)//Code Here
 
 
 
@@ -61,16 +61,22 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter() {
+  let count = 0
+  function addOne() {
+    return (count += 1)
+  }
+  return addOne
+}//Code Here
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -86,10 +92,16 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
+  
   // Code here.
 
   return {
-
+    inc : function() {
+      return (value += 1)
+    },
+    dec : function() {
+      return (value -= 1)
+    }
   };
 }
 
@@ -112,10 +124,12 @@ counter = counterFactory(10);
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
 
-  // code message function here.
+  function message() {
+    return `You're doing awesome, keep it up ${firstname} ${lastname}.`
+  }// code message function here.
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,7 +157,9 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod: function() {
+      return privateMethod()
+    }// Code here.
   };
 })();
 
@@ -162,7 +178,12 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret : function(number) {
+      return (secret += number)
+    },
+    takeAwayFromSecret : function(number) {
+      return (secret -= number)
+    }// Code here
   };
 }
 
@@ -187,7 +208,7 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 5; i++) {
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
